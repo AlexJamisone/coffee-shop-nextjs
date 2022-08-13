d5820b2fabd7b45ce506988080ce574d2fa6aa8d
AlexJamisone <business.homeit@gmail.com>
2022-08-13 19:19:36 +0300

    clean up app, learn fetures

---
+++ pages/_app.jsx
@@ -0,0 +1,14 @@
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+	return (
+	  <>
+		<Component {...pageProps} />
+			<footer>
+				<p>© 2022 Alex Jamison</p>
+			</footer>
+	  </>
+  )
+}
+
+export default MyApp
---
+++ pages/world.jsx
@@ -0,0 +1,5 @@
+const World = () => {
+    return <div>Hello World Component</div>
+}
+
+export default World
